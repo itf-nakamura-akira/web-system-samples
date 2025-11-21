8dcb6d24e0daa65a3d33c47068af553d889449f7
Akira Nakamura <nakamura.akira@itfllc.co.jp>
2025-11-21 11:20:36 +0000

    feat(auth): Return JWT tokens on successful login
    Integrates JWT generation into the login process.
    
    Upon successful authentication, the login endpoint now returns an access token and a refresh token instead of user information.
    
    - Modified LoginController to use JwtUtils for token generation.
    - Updated the PostLoginResponse to include accessToken and refreshToken.

--- packages/spring-boot/src/main/java/jp/co/itfllc/WebSystemSamples/features/login/LoginController.java
+++ packages/spring-boot/src/main/java/jp/co/itfllc/WebSystemSamples/features/login/LoginController.java
@@ -1,8 +1,10 @@
 package jp.co.itfllc.WebSystemSamples.features.login;
 
+import java.util.HashMap;
 import jp.co.itfllc.WebSystemSamples.features.login.models.PostLoginRequest;
 import jp.co.itfllc.WebSystemSamples.features.login.models.PostLoginResponse;
 import jp.co.itfllc.WebSystemSamples.mappers.results.entities.UsersEntity;
+import jp.co.itfllc.WebSystemSamples.utils.JwtUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,17 +18,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoginController {
 
     /**
-     * ログイン機能用 Service
+     * ログイン画面用 Service
      */
     private final LoginService loginService;
+
+    /**
+     * JWTユーティリティクラス
+     */
+    private final JwtUtils jwtUtils;
 
     /**
      * コンストラクタ
      *
      * @param loginService ログイン機能用 Service
+     * @param jwtUtils JWTユーティリティ
      */
-    public LoginController(LoginService loginService) {
+    public LoginController(LoginService loginService, JwtUtils jwtUtils) {
         this.loginService = loginService;
+        this.jwtUtils = jwtUtils;
     }
 
     /**
@@ -40,8 +49,10 @@ public class LoginController {
         // ログイン認証
         UsersEntity loginUser = this.loginService.login(request.account(), request.password());
 
-        // TODO JWTの生成
+        // アクセストークンとリフレッシュトークンを生成します
+        String accessToken = this.jwtUtils.generateAccessToken(loginUser.getAccount(), new HashMap<>());
+        String refreshToken = this.jwtUtils.generateRefreshToken(loginUser.getAccount(), new HashMap<>());
 
-        return new PostLoginResponse(loginUser.getName(), loginUser.getHashedPassword());
+        return new PostLoginResponse(accessToken, refreshToken);
     }
 }
