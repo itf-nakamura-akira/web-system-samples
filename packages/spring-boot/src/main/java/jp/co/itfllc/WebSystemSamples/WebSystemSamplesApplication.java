0d842eab3681bd483aed1a68d6fed1e6d1671d54
Akira Nakamura <nakamura.akira@itfllc.co.jp>
2025-11-21 11:20:36 +0000

    feat: Add initial Spring Boot project structure

---
+++ packages/spring-boot/src/main/java/jp/co/itfllc/WebSystemSamples/WebSystemSamplesApplication.java
@@ -0,0 +1,20 @@
+package jp.co.itfllc.WebSystemSamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Web System Samples クラス
+ */
+@SpringBootApplication
+public class WebSystemSamplesApplication {
+
+    /**
+     * エントリーポイント
+     *
+     * @param args 起動引数
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(WebSystemSamplesApplication.class, args);
+    }
+}
---
+++ packages/spring-boot/src/test/java/jp/co/itfllc/WebSystemSamples/WebSystemSamplesApplicationTests.java
@@ -0,0 +1,11 @@
+package jp.co.itfllc.WebSystemSamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSystemSamplesApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
