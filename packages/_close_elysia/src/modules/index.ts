85e848c701b680ec85fa8c3f99076b4d605b8ff2
Akira Nakamura <nakamura.akira@itfllc.co.jp>
2025-11-22 06:31:56 +0000

    chore(elysia): delete _close_elysia package

--- packages/_close_elysia/src/modules/index.ts
+++
@@ -1,5 +0,0 @@
-import Elysia from 'elysia';
-import { common } from './common/common.controller';
-import { login } from './login/login.controller';
-
-export const modules = new Elysia({ prefix: '/api' }).use([login, common]);
